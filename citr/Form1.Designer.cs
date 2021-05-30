2b6b545e9fae5ac0358644a59b74c8ba48259f78
nikitaka7 <nikitaka7@mail.ru>
2021-05-30 17:09:10 +0500

    release v1

---
+++ citr/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace citr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(377, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(371, 115);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать (.bmp)";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(377, 265);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(371, 94);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Рассчитать";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Display", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(431, 362);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Результат:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sitka Display", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(396, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Путь к файлу:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Sitka Small", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(36, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 576);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 614);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ citr/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace citr
+{
+    public partial class Form1 : Form
+    {
+        string path = "";
+        string filter = "Image Files (*.bmp)|*.bmp";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = filter;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                label2.Text = ofd.FileName;
+                path = ofd.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (path == "")
+            {
+                return;
+            }
+            Bitmap img = (Bitmap)Image.FromFile(@path);
+            bool[,] pik0 = new bool[img.Height, img.Width];
+            Color pix = img.GetPixel(0, 0);
+            int minx = img.Height, miny = img.Width, maxx = 0, maxy = 0;
+            for (int i = 0; i < img.Height; i++)
+            {
+                for (int j = 0; j < img.Width; j++)
+                {
+                    if (img.GetPixel(j, i) != pix)
+                    {
+                        pik0[i, j] = true;
+                        minx = Math.Min(minx, i);
+                        miny = Math.Min(miny, j);
+                        maxx = Math.Max(maxx, i);
+                        maxy = Math.Max(maxy, j);
+                    }
+                }
+            }
+            // вот тут уменьшим картинку чтобы были точнее резы
+            int width = maxy - miny + 1;
+            int height = maxx - minx + 1;
+            bool[,] pik = new bool[height, width];
+            for (int i = minx; i <= maxx; i++)
+            {
+                for (int j = miny; j <= maxy; j++)
+                {
+                    pik[i - minx, j - miny] = pik0[i, j];
+                }
+            }
+            // закончили уменьшать картинку
+
+            int deep = 10;
+            int[] points = new int[deep];
+            int start = 5;
+            int g = 0;
+            for (int k = start; k < start + deep; k++) // кол-во шагов
+            {
+                g = 0;
+                for (int i = 0; i <= height / k; i++) // квадраты большие
+                {
+                    for (int j = 0; j <= width / k; j++) // квадраты большие
+                    {
+                        for (int ii = 0; ii < k; ii++)  // квадраты малые
+                        {
+                            if (k * i + ii >= height)
+                            {
+                                continue;
+                            }
+                            for (int jj = 0; jj < k; jj++)  // квадраты малые
+                            {
+                                if (k * j + jj >= width)
+                                {
+                                    continue;
+                                }
+                                if (pik[k * i + ii, k * j + jj])
+                                {
+                                    g++;
+                                    jj = k;
+                                    ii = k;
+                                }
+                            }
+                        }
+                    }
+                }
+                points[k - start] = g;
+                if (g == 1)
+                {
+                    deep = k - start;
+                }
+            }
+            double[,] loge = new double[2, deep];
+            for (int i = 0; i < deep; i++)
+            {
+                loge[0, i] = Math.Log(start + i);
+                loge[1, i] = Math.Log(points[i]);
+            }
+            double sumx = 0, sumy = 0, sumxy = 0, sumxx = 0;
+            for (int i = 0; i < deep; i++)
+            {
+                sumx += loge[0, i];
+                sumy += loge[1, i];
+                sumxx += loge[0, i] * loge[0, i];
+                sumxy += loge[0, i] * loge[1, i];
+            }
+            label1.Text = (-(sumxy - sumy * sumx / deep) / (sumxx - sumx * sumx / deep)).ToString();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
